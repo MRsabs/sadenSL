5e9b8d5b83f4e2b504b71874e62a90d201450d31
MRsabs <mdiq287@gmail.com>
2020-06-28 19:21:45 +0300

    added unix_now method

--- addon/native/src/lib.rs
+++ addon/native/src/lib.rs
@@ -1,3 +1,4 @@
+use chrono::prelude::*;
 use neon::prelude::*;
 use num_format::{Locale, ToFormattedString};
 
@@ -12,8 +13,15 @@ fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
     Ok(cx.string("from rust"))
 }
 
+fn unix_now(mut cx: FunctionContext) -> JsResult<JsNumber> {
+    let x = chrono::Utc::now().to_rfc2822().to_string();
+    let x = chrono::DateTime::parse_from_rfc2822(x.as_str()).unwrap();
+    Ok(cx.number(x.timestamp() as f64))
+}
+
 register_module!(mut m, {
     m.export_function("nwc", nwc)?;
     m.export_function("hello", hello)?;
+    m.export_function("unixNow", unix_now)?;
     Ok(())
 });

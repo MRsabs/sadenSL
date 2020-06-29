16aebf3ea1de96404dd07814511127c0f09fa3d7
MRsabs <mdiq287@gmail.com>
2020-06-29 10:50:49 +0300

    added uuid_v4

--- addon/native/src/lib.rs
+++ addon/native/src/lib.rs
@@ -1,16 +1,21 @@
-use chrono::prelude::*;
+// use chrono::prelude::*;
 use neon::prelude::*;
 use num_format::{Locale, ToFormattedString};
+
+fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
+    Ok(cx.string("Hello from rust"))
+}
+
+fn uuid_v4(mut cx: FunctionContext) -> JsResult<JsString> {
+    let x = uuid::Uuid::new_v4().to_simple().to_string();
+    Ok(cx.string(x))
+}
 
 fn nwc(mut cx: FunctionContext) -> JsResult<JsString> {
     let x = cx.argument::<JsString>(0)?.value();
     let x = x.parse::<u32>().unwrap();
     let x = x.to_formatted_string(&Locale::en);
     Ok(cx.string(x))
-}
-
-fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
-    Ok(cx.string("from rust"))
 }
 
 fn unix_now(mut cx: FunctionContext) -> JsResult<JsNumber> {
@@ -23,5 +28,6 @@ register_module!(mut m, {
     m.export_function("nwc", nwc)?;
     m.export_function("hello", hello)?;
     m.export_function("unixNow", unix_now)?;
+    m.export_function("uuidV4", uuid_v4)?;
     Ok(())
 });

fc232fa38d6def50b654cd01b916a46ede005e97
MRsabs <mdiq287@gmail.com>
2020-06-30 18:48:01 +0300

    added init functions

--- src/db/init.js
+++ src/db/init.js
@@ -1,33 +1,53 @@
-/* eslint-disable */
 const Sequelize = require('sequelize');
 const path = require('path');
 const { app } = require('electron');
 const isDev = require('electron-is-dev');
-const { Model, DataTypes } = require('sequelize');
 
 const sequelize = new Sequelize({
+  logging: false,
   dialect: 'sqlite',
   storage: isDev
     ? path.join(process.cwd(), 'sadensl.sqlite')
-    : path.join(app.getPath('userData'), 'saden-sl.sqlite'),
+    : path.join(app.getPath('userData'), 'sadensl.sqlite'),
 });
 
-(async function () {
+export async function testDb() {
+  try {
+    const models = await import('./models/index');
+    const tCustomer = await models.Customer.create({ name: 'james' });
+    await models.Order.create({
+      customerId: tCustomer.getDataValue('id'),
+      product: 'someProduct',
+    });
+    return true;
+  } catch (error) {
+    // TODO log erro to a file
+    console.error('inseting to database failed');
+    return false;
+  }
+}
+
+export async function syncDb() {
+  try {
+    await import('./models/index');
+    await sequelize.sync({ alter: { drop: false } });
+    return true;
+  } catch (error) {
+    // TODO log erro to a file
+    console.error('database did not sync');
+    return false;
+  }
+}
+
+export async function authenticateDb() {
   try {
     await sequelize.authenticate();
-    const models = await import('./models/index');
-    await sequelize.sync({ alter: { drop: false } });
-    const jane = await models.Customer.create({
-      name: 'james',
-    }).then((customer) => {
-      models.Order.create({
-        customerId: customer.getDataValue('id'),
-        product: 'someProduct',
-      });
-    });
+    return true;
   } catch (error) {
-    console.error(error);
+    // TODO log erro to a file
+    console.error('database did not authenticate');
+    return false;
   }
-})();
+}
 
 export default sequelize;

b62d0f058937fe51917ace3ede788018b568c0ba
MRsabs <mdiq287@gmail.com>
2020-07-13 04:13:48 +0300

    idk

---
+++ renderer/webpack.config.prod.js
@@ -0,0 +1,55 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const fs = require('fs-extra');
+if (fs.existsSync(path.join(__dirname, '../dist/renderer'))) {
+  fs.removeSync(path.join(__dirname, '../dist/renderer'));
+}
+
+module.exports = {
+  entry: path.resolve(__dirname, './src/index.jsx'),
+  mode: 'production',
+  target: 'electron-renderer',
+  devtool: 'eval',
+  output: {
+    path: path.resolve(__dirname, '../dist/renderer'),
+    filename: 'index_bundle.js',
+    publicPath: './',
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './dist',
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+  ],
+  resolve: {
+    extensions: ['.wasm', '.js', '.jsx', '.json'],
+    alias: {
+      '@utils': path.join(__dirname, './src/utils'),
+      '@contexts': path.join(__dirname, './src/contexts'),
+    },
+  },
+};

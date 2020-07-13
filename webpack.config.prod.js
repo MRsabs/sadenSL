1ca3abb5266d1a67cf7dc2be8eb4fea847326d34
MRsabs <mdiq287@gmail.com>
2020-07-13 04:13:12 +0300

    webpack for dev and prod configs

---
+++ webpack.config.prod.js
@@ -0,0 +1,41 @@
+const Path = require('path');
+const fs = require('fs-extra');
+const nodeExternals = require('webpack-node-externals');
+if (fs.existsSync(Path.resolve(__dirname, './tmp'))) {
+  fs.removeSync(Path.resolve(__dirname, './tmp'));
+}
+
+module.exports = {
+  mode: 'production',
+  node: {
+    __dirname: false,
+  },
+  externals: [nodeExternals()],
+  target: 'electron-main',
+  entry: Path.join(__dirname, './src/main.ts'),
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+        options: {
+          fix: true,
+        },
+      },
+      { test: /\.ts$/, exclude: /node_modules/, loader: 'ts-loader' },
+      {
+        test: /\.node$/,
+        loader: 'node-loader',
+      },
+    ],
+  },
+  output: {
+    path: Path.join(__dirname, '/dist'),
+  },
+  resolve: {
+    extensions: ['.wasm', '.js', '.ts', '.json', '.node'],
+  },
+  watch: false,
+};

ca82b2db8584ecdced81e85024d119c25c469c6b
MRsabs <mdiq287@gmail.com>
2020-07-15 05:19:41 +0300

    improved nwc func to handle strings and numbers

--- src/ipc/helpers.ts
+++ src/ipc/helpers.ts
@@ -1,8 +1,34 @@
 import { ipcMain } from 'electron-better-ipc';
 import { nwc, unixNow } from '../util/native';
 
-ipcMain.handle('nwc', (e, x: string): string => {
-  return nwc(x);
+ipcMain.handle('nwc', (e, x: string | number): string => {
+  console.log('x is: ', x)
+
+  function towDesimlsNum(x: string | number): string {
+    const num = Number(x)
+    if (num % 1 != 0) {
+      return num.toFixed(2).toString()
+    } else {
+      return num.toString() + '.00'
+    }
+  }
+
+  if (typeof x === 'number') {
+    const toFixed2 = towDesimlsNum(x);
+    const intAndDes = toFixed2.split('.');
+    const formatedNum = nwc(intAndDes[0]);
+    const numWithDesimls = formatedNum + '.' + intAndDes[1];
+    return numWithDesimls;
+  } else if (typeof x === 'string') {
+    const toFixed2 = towDesimlsNum(x);
+    const intAndDes = toFixed2.split('.');
+    const formatedNum = nwc(intAndDes[0]);
+    const numWithDesimls = formatedNum + '.' + intAndDes[1];
+    return numWithDesimls;
+  } else {
+    // TODO High Risk code injection
+    return null;
+  }
 });
 
 ipcMain.handle('unixNow', async () => {

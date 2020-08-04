014c1d1ba29cc3232a13ffc49f5454df5ccd4d1f
MRsabs <mdiq287@gmail.com>
2020-08-04 20:47:06 +0300

    feat: storage route index

---
+++ renderer/src/pages/storage/index.tsx
@@ -0,0 +1,32 @@
+import React, { useContext, useEffect } from 'react';
+import { StorageContext } from '@contexts/StorageContext';
+import { ipcRenderer } from 'electron';
+import NoStorage from './noStorage/NoStorage';
+import StorageManger from './StorageManger';
+import OneStorageContextProvider from '@contexts/OneStorageContext';
+
+export default function Storage(): JSX.Element {
+  const { state, dispatch } = useContext(StorageContext);
+  useEffect(() => {
+    ipcRenderer.invoke('inventory/read/all').then((data) => {
+      dispatch({
+        type: 'sync',
+        payload: data,
+      });
+    });
+  }, []);
+
+  if (state.count === 0) {
+    return <NoStorage />;
+  } else {
+    return <StorageMangerWithContext />;
+  }
+}
+
+function StorageMangerWithContext(): JSX.Element {
+  return (
+    <OneStorageContextProvider>
+      <StorageManger />
+    </OneStorageContextProvider>
+  );
+}

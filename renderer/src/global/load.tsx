e3c574a2e154de0eb0740c25dcb0c75a99f1e2dc
MRsabs <mdiq287@gmail.com>
2020-08-07 15:42:12 +0300

    fix: fix index.css path

--- renderer/src/global/load.tsx
+++ renderer/src/global/load.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { hot } from 'react-hot-loader/root';
 import { BrowserRouter as Router } from 'react-router-dom';
 import 'antd/dist/antd.css';
-import './index.css';
+import '../index.css';
 import App from './App';
 import Init from './Init';
 import GlobalContextProvider from '@contexts/GlobalContext';

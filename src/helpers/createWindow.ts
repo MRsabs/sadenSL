01e2d6fa854240c7d927c19d5397499d8e062d54
MRsabs <mdiq287@gmail.com>
2020-08-07 15:43:49 +0300

    feat: added React dev tools

--- src/helpers/createWindow.ts
+++ src/helpers/createWindow.ts
@@ -1,7 +1,7 @@
 import { BrowserWindow } from 'electron';
 import * as path from 'path';
+import * as os from 'os';
 import isDev from 'electron-is-dev';
-
 
 export default function createWindow(
   winOpt: Electron.BrowserWindowConstructorOptions
@@ -10,8 +10,19 @@ export default function createWindow(
   if (isDev) {
     mainWindow.loadURL('http://localhost:8080');
     mainWindow.webContents.openDevTools();
+    loadDevTools(mainWindow);
   } else {
     mainWindow.loadFile(path.join(__dirname, './erenderer/index.html'));
   }
   return mainWindow;
+}
+
+function loadDevTools(window: BrowserWindow) {
+  const ReactDevTool = path.join(
+    os.homedir(),
+    '.config/google-chrome/Default/Extensions/fmkadmapgofadopljbjfkapdkoienihi/4.8.2_0/'
+  );
+  window.webContents.session
+    .loadExtension(ReactDevTool)
+    .catch((err) => console.error(err));
 }

f7ac7bcffd1d4afb6839a85c87ef3582af09b278
MRsabs <mdiq287@gmail.com>
2020-08-12 21:21:15 +0300

    feat: added types

--- renderer/src/contexts/OneStorageContext.tsx
+++ renderer/src/contexts/OneStorageContext.tsx
@@ -1,8 +1,15 @@
 import React, { createContext, useReducer } from 'react';
 
-export const OneStorageContext = createContext(null);
+export const OneStorageContext = createContext<{
+  state: ProductsInfo[];
+  dispatch: React.Dispatch<ReducerAction>;
+}>(null);
 
-const oneStorageReducer = (state, action) => {
+function init(): [] {
+  return [];
+}
+
+const oneStorageReducer = (state: ProductsInfo[], action: ReducerAction) => {
   switch (action.type) {
     case 'sync':
       return action.payload;
@@ -14,7 +21,7 @@ const oneStorageReducer = (state, action) => {
 const OneStorageContextProvider = (props: {
   children: React.ReactNode;
 }): JSX.Element => {
-  const [state, dispatch] = useReducer(oneStorageReducer, [{}]);
+  const [state, dispatch] = useReducer(oneStorageReducer, [{}], init);
   return (
     <OneStorageContext.Provider value={{ state, dispatch }}>
       {props.children}
@@ -23,3 +30,25 @@ const OneStorageContextProvider = (props: {
 };
 
 export default OneStorageContextProvider;
+
+// types
+export interface ProductsInfo {
+  createdAt: number;
+  id: string;
+  'product.barcode': string;
+  'product.createdAt': number;
+  'product.id': string;
+  'product.name': string;
+  'product.retailPrice': number;
+  'product.updatedAt': number;
+  'product.wholeSalePrice': number;
+  productId: string;
+  quantityInStock: number;
+  trackerId: string;
+  updatedAt: number;
+}
+
+interface ReducerAction {
+  type: 'sync';
+  payload: ProductsInfo[];
+}

66f9e645c7b91d5d71c7487c967868ce6b6be00d
MRsabs <mdiq287@gmail.com>
2020-08-12 21:28:35 +0300

    feat: added order/read/all and fix order/create

--- src/ipc/db/order.ts
+++ src/ipc/db/order.ts
@@ -1,41 +1,100 @@
-import { ipcMain as ipc } from 'electron';
-// import { nwc, unixNow } from '../util/native';
-import * as models from '../../db/models/index';
+import { ipcMain } from 'electron';
+import {
+  Customer,
+  Order,
+  OrderProduct,
+  Inventory,
+} from '../../db/models/index';
 
-ipc.handle('order/create', async (e, orderData: any) => {
-  try {
-    const customer = await models.Customer.findOne({
-      where: {
-        id: orderData.customerId,
-      },
-    });
-    const order = await models.Order.create({
-      customerId: customer.getDataValue('id'),
-    });
-    const orderProduct = await models.OrderProduct.create({
-      orderId: order.getDataValue('id'),
-      productId: orderData.productId,
-      quantity: orderData.quantity,
-    });
-    const updateQuantity = await models.Inventory.findOne({
-      where: {
-        productId: orderData.productId,
-      },
-    });
-    const quantityInStock = updateQuantity.getDataValue('quantityInStock');
-    const quantityInOrder = orderProduct.getDataValue('quantity');
-    const newQuantity = quantityInStock - quantityInOrder;
-    await models.Inventory.update(
-      { quantityInStock: newQuantity },
-      {
+ipcMain.on(
+  'order/create',
+  async (
+    e,
+    orderData: OrderData[],
+    customerData: CustomerData = { id: 'guest' }
+    // total: number
+  ) => {
+    try {
+      const customer = await Customer.findOne({
         where: {
-          productId: orderProduct.getDataValue('productId'),
+          id: customerData.id,
         },
+      });
+      const order = await Order.create({
+        customerId: customer.getDataValue('id'),
+      });
+      for (let i = 0; i < orderData.length; i++) {
+        const item: OrderData = orderData[i];
+        const updateQuantity = await Inventory.findOne({
+          where: {
+            productId: item.key,
+          },
+        });
+
+        const orderProduct = await OrderProduct.create({
+          orderId: order.getDataValue('id'),
+          productId: item.key,
+          quantity: item.unitsOrdered,
+        });
+        const quantityInStock = updateQuantity.getDataValue('quantityInStock');
+        const quantityInOrder = orderProduct.getDataValue('quantity');
+        const newQuantity = quantityInStock - quantityInOrder;
+        await Inventory.update(
+          { quantityInStock: newQuantity },
+          {
+            where: {
+              productId: orderProduct.getDataValue('productId'),
+            },
+          }
+        );
       }
-    );
-    return true;
+      e.returnValue = true;
+    } catch (error) {
+      // TODO log error & display friendly message
+      console.error(error);
+      e.returnValue = false;
+    }
+  }
+);
+
+ipcMain.handle('order/read/all', async (e) => {
+  try {
+    const data: Order[] = await Order.findAll();
+    const result: AllOrders[] = [];
+    for (let i = 0; i < data.length; i++) {
+      const value = data[i];
+      const val = value.toJSON() as AllOrders;
+      const customer = await Customer.findOne({
+        where: { id: val.customerId },
+      });
+      val.customerName = await customer.getDataValue('name');
+      result.push(val);
+    }
+    return result;
   } catch (error) {
     // TODO log error & display friendly message
+    console.error(error);
     return false;
   }
 });
+
+// types
+interface OrderData {
+  key: string;
+  productName: string;
+  unitPrice: number;
+  unitsOrdered: number;
+  totalUnitsPrice: number;
+}
+
+interface CustomerData {
+  id: string;
+}
+
+interface AllOrders {
+  customerName: string;
+  id: string;
+  createdAt: number;
+  updatedAt: number;
+  customerId: string;
+}

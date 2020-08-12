7c2397f0503c980d542abd1ceac839a45ba55b87
MRsabs <mdiq287@gmail.com>
2020-08-12 21:18:40 +0300

    feat: added NWC func

--- renderer/wasm/src/lib.rs
+++ renderer/wasm/src/lib.rs
@@ -1,25 +1,16 @@
-mod utils;
 use num_format::{Locale, ToFormattedString};
 use wasm_bindgen::prelude::*;
 
-// // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// // allocator.
-// #[cfg(feature = "wee_alloc")]
-// #[global_allocator]
-// static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
-// #[wasm_bindgen]
-// extern {
-//     fn alert(s: &str);
-// }
-
 #[wasm_bindgen]
-pub fn nwc(number: i32) -> String {
-    let number = number.to_formatted_string(&Locale::en);
-    return number;
+pub fn nwc(number: JsValue) -> JsValue {
+    let number = number.as_f64();
+    match number {
+        None => JsValue::from_bool(false),
+        Some(quotient) => {
+            let tmp = quotient as i64;
+            let x = tmp.to_formatted_string(&Locale::en);
+            let x = JsValue::from(x);
+            return x;
+        }
+    }
 }
-
-// #[wasm_bindgen]
-// pub fn greet() -> f64 {
-//     return 10 as f64;
-// }

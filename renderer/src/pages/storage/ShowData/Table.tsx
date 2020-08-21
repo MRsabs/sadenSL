fb4fe40ff76ad8bf32b03a5ba91711f76d12cd8b
MRsabs <mdiq287@gmail.com>
2020-08-21 14:17:42 +0300

    feat: Table components

--- renderer/src/pages/storage/ShowData/Table.tsx
+++ renderer/src/pages/storage/ShowData/Table.tsx
@@ -1,7 +1,12 @@
-import React, { useLayoutEffect, useState } from 'react';
-import { Table as AntTable } from 'antd';
-import { ColumnsType } from 'antd/es/table';
+import React, { useLayoutEffect, useState, useContext } from 'react';
 import { ProductsInfo } from '@contexts/OneStorageContext';
+import OrderForm from 'src/components/OrderForm';
+import { StorageContext } from '@contexts/StorageContext';
+import AntTable from 'antd/lib/table';
+import Column from 'antd/lib/table/Column';
+import ColumnGroup from 'antd/lib/table/ColumnGroup';
+import { numberWithCommas } from 'src/global/funcs';
+import moment from 'moment';
 
 function getContentSectionHeight(): number {
   return document.querySelector('#root > section > section > section > main')
@@ -10,43 +15,18 @@ function getContentSectionHeight(): number {
 
 function GetTableHeight(ContentSectionHeight: number) {
   // ContentSectionHeight - tableHeader - TableButtom - 2 * paddingY
-  const TableHeight = ContentSectionHeight - 38 - 64 - 2 * 24;
+  const TableHeight = ContentSectionHeight - 38 - 64 - 2 * 24 - 85;
   return TableHeight;
 }
 
-const columns: ColumnsType<Product> = [
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    sorter: (a, b) => a.name.length - b.name.length,
-    sortDirections: ['descend', 'ascend'],
-  },
-  {
-    title: 'Barcode',
-    dataIndex: 'barcode',
-  },
-  {
-    title: 'Retail Price',
-    dataIndex: 'retailPrice',
-    defaultSortOrder: 'descend',
-    sorter: (a, b) => a.retailPrice - b.retailPrice,
-    sortDirections: ['descend', 'ascend'],
-  },
-  {
-    title: 'Whole Sale Price',
-    dataIndex: 'wholeSalePrice',
-    defaultSortOrder: 'descend',
-    sorter: (a, b) => a.wholeSalePrice - b.wholeSalePrice,
-    sortDirections: ['descend', 'ascend'],
-  },
-  {
-    title: 'Quantity In Stock',
-    dataIndex: 'quantityInStock',
-  },
-];
-
 function Table(props: Props): JSX.Element {
   const [tableHeight, setTableHeight] = useState(0);
+  const storageSelected = useContext(StorageContext).state.rows.map(
+    ({ dataValues: { id, inventoryName } }) => ({
+      id,
+      inventoryName,
+    })
+  );
 
   useLayoutEffect(() => {
     function updateSize() {
@@ -64,6 +44,7 @@ function Table(props: Props): JSX.Element {
 
   function dataArr(): Product[] {
     return props.data.map((val) => {
+      console.log(val);
       const res = {
         key: val.id,
         name: val['product.name'],
@@ -71,6 +52,9 @@ function Table(props: Props): JSX.Element {
         retailPrice: val['product.retailPrice'],
         quantityInStock: val['quantityInStock'],
         barcode: val['product.barcode'],
+        productionDate: val['product.productionDate'],
+        expirationDate: val['product.expirationDate'],
+        notes: val['notes'],
       };
       return res;
     });
@@ -82,10 +66,65 @@ function Table(props: Props): JSX.Element {
         size="small"
         pagination={{ pageSize: 20 }}
         scroll={{ y: `${tableHeight}px` }}
-        columns={columns}
+        // columns={columns}
         dataSource={dataArr()}
         onChange={onChange}
-      />
+        title={() => <OrderForm trackerSelected={storageSelected} />}
+      >
+        <Column title="Product Name" dataIndex="name" key="name" />
+        <Column title="Barcode" dataIndex="barcode" key="barcode" />
+        <Column
+          title="Quantity"
+          dataIndex="quantityInStock"
+          key="quantityInStock"
+          render={(value) => {
+            return numberWithCommas(value);
+          }}
+        />
+        <ColumnGroup title={'Pricing'}>
+          <Column
+            title="Retail Price"
+            dataIndex="retailPrice"
+            key="retailPrice"
+            render={(value) => {
+              return numberWithCommas(value, {
+                options: { currency: 'IQD', style: 'currency' },
+              });
+            }}
+          />
+          <Column
+            title="Whole Sale Price"
+            dataIndex="wholeSalePrice"
+            key="wholeSalePrice"
+            render={(value) => {
+              return numberWithCommas(value, {
+                options: { currency: 'IQD', style: 'currency' },
+              });
+            }}
+          />
+        </ColumnGroup>
+        <ColumnGroup title={'P & E'}>
+          <Column
+            title="Production Date"
+            dataIndex="productionDate"
+            key="productionDate"
+            render={(value) => {
+              return moment.unix(value).format('YYYY-MM-DD');
+            }}
+          />
+          <Column
+            title="Expiration Date"
+            dataIndex="expirationDate"
+            key="expirationDate"
+            render={(value) => {
+              return moment.unix(value).format('YYYY-MM-DD');
+              // return `${moment.unix(value).format('YYYY-MM-DD')}\n${moment
+              //   .unix(value)
+              //   .format('YYYY-MM-DD')}`;
+            }}
+          />
+        </ColumnGroup>
+      </AntTable>
     </>
   );
 }
@@ -102,4 +141,7 @@ interface Product {
   wholeSalePrice: number;
   retailPrice: number;
   quantityInStock: number;
+  productionDate: number;
+  expirationDate: number;
+  notes: string;
 }
